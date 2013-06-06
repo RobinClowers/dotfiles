bd7fe6fa0e1d7c94a655f328e882ae5df9d1dae2
Robin Clowers <robin.clowers@gmail.com>
2013-06-05 22:12:33 -0700

    Improve link script output

--- link.sh
+++ link.sh
@@ -6,5 +6,5 @@ SCRIPTNAME=$(basename $0)
 cd $SCRIPTDIR
 for FILE in *
 do
-  [ $FILE != $SCRIPTNAME ] && ln -s $SCRIPTDIR/$FILE ~/.$FILE
+  [ $FILE != $SCRIPTNAME ] && ln -s $SCRIPTDIR/$FILE ~/.$FILE 2> /dev/null && echo "linked $SCRIPTDIR/$FILE to ~/.$FILE"
 done
