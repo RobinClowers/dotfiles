b30e642aba84da35757de509d501176156224cfc
Robin Clowers <robin.clowers@gmail.com>
2017-06-14 09:57:00 -0700

    Add karabiner.json

--- link.sh
+++ link.sh
@@ -8,3 +8,6 @@ for FILE in *
 do
   [ $FILE != $SCRIPTNAME ] && ln -s $SCRIPTDIR/$FILE ~/.$FILE 2> /dev/null && echo "linked $SCRIPTDIR/$FILE to ~/.$FILE"
 done
+
+# symlinks don't seem to work for this file
+cp misc/karabiner.json ~/.config/karabiner/
