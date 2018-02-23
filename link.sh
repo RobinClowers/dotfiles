a70693e9921ad67bfdb54fc40e2c0d5b961d3354
Robin Clowers <robin.clowers@gmail.com>
2018-02-22 18:46:08 -0800

    Don't try to link when target exists

--- link.sh
+++ link.sh
@@ -4,9 +4,18 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 SCRIPTNAME=$(basename $0)
 
 cd $SCRIPTDIR
+
 for FILE in *
 do
-  [ $FILE != $SCRIPTNAME ] && ln -s $SCRIPTDIR/$FILE ~/.$FILE 2> /dev/null && echo "linked $SCRIPTDIR/$FILE to ~/.$FILE"
+  TARGET="$HOME/.$FILE"
+
+  [ $FILE != $SCRIPTNAME ] &&
+    if [ -e $TARGET ]; then
+      [ ! -h $TARGET ] && echo "$TARGET exists, please remove it first"
+    else
+      ln -s $SCRIPTDIR/$FILE ~/.$FILE 2> /dev/null &&
+      echo "linked $SCRIPTDIR/$FILE to ~/.$FILE"
+    fi
 done
 
 # symlinks don't seem to work for this file
