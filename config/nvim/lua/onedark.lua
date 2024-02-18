40faf9036778014402a4c3b4c2ff422f99947884
Robin Clowers <robin.clowers@gmail.com>
2024-02-18 14:21:24 -0800

    Plugins working

---
+++ config/nvim/lua/onedark.lua
@@ -0,0 +1,27 @@
+require("onedark").setup  {
+  -- Main options --
+  style = "darker", -- Default theme style. Choose between "dark", "darker", "cool", "deep", "warm", "warmer" and "light"
+
+  -- Custom Highlights --
+  colors = {
+    bg0 = "#1C1D1B",
+  }, -- Override default colors
+
+  highlights = {
+    ["@keyword"] = {fg = "$red"},
+    ["@keyword.conditional"] = {fg = "$purple"},
+    ["@keyword.directive"] = {fg = "$red"},
+    ["@keyword.import"] = {fg = "$red"},
+    ["@string"] = {fg = "$yellow"},
+    ["@string.escape"] = {fg = "$grey"},
+    ["@string.special"] = {fg = "$grey"},
+    ["@tag"] = {fg = "$green" },
+    ["@tag.attribute"] = {fg = "$blue"},
+    ["@tag.delimiter"] =  {fg = "$grey" },
+    ["@type"] = {fg = "$green"},
+    ["@variable.parameter"] = {fg = "$cyan"},
+  }, -- Override highlight groups
+}
+
+
+require("onedark").load()
